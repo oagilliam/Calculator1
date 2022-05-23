f1b3209e8e167445ec8d6b87f1f18bb4c7ff4e81
oagilliam <75933763+oagilliam@users.noreply.github.com>
2022-05-22 20:35:16 -0400

    Create Calculator1.py

---
+++ Calculator1.py
@@ -0,0 +1,9 @@
+#5/22/2022
+#Prompt: Make a calculator that adds two entered numbers, then displays the result.
+
+
+num1 = int(input('>>')) #Recieves the first number
+num2 = int(input('>>')) #Recieves the second number
+result = num1 + num2    #Calculaets the sum of the first 2 inputs
+
+print(result) #Displays the result of the sum
